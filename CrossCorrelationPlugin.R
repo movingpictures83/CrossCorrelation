f81791b2b9d3c6143ccd8d0013158e4cdff05a0d
Trevor Cickovski <tcickovs@fiu.edu>
2018-01-22 10:35:38 -0500

    Changed plugin to assume input in a single CSV, rather than the more challenging prefix option More detailed README Simple example

--- CrossCorrelationPlugin.R
+++ CrossCorrelationPlugin.R
@@ -1,28 +1,17 @@
 library('stats')
 
 input <- function(inputfile) {
-  x <<- scan(paste(inputfile,"1.txt",sep=''));
-  y <<- scan(paste(inputfile,"2.txt",sep=''));
+   mydata <<- read.csv(inputfile, header=FALSE);
 }
 
 
 run <- function() {
-   results <<-  ccf(x,y);
+   mydata <<- mydata[,-1];
+   results <<-  ccf(mydata[1],mydata[2]);
 }
 
 output <- function(outputfile) {
-   sink(outputfile, append=TRUE);
-   cat("LAG\tACF\n");
-   #fileConn <- file(outputfile);
-   for (index in 1:length(results$lag)) {
-      cat(paste(toString(results$lag[index]), "\t", toString(results$acf[index]), "\n"));
-   }
-   #writeLines(paste("Dickey-Fuller:\t", toString(results$statistic),
-   #           "\n", "Method:\t", results$method,
-   #           "\n", "Lag Order:\t", results$parameter,
-   #           "\n", "P-Value:\t", toString(results$p.value)), fileConn);
-   sink();
-   #close(fileConn);
+   write.table(results$acf, file=outputfile);
 }
 
 
--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # CrossCorrelation
 # Language: R
-# Input: prefix (for time series datasets)
+# Input: CSV (time series datasets)
 # Output: TXT (cross-correlation value)
-PluMA plugin to compute cross-correlation values between time-series datasets
+
+PluMA plugin to compute cross-correlation between two time-series datasets.
+The plugin accepts CSV file input and assumes three columns: the first is the
+name of each time-series sample, the second column is the first dataset, and the
+third column is the second dataset.  It thus assumes each row in the file to be
+one time-series sample.
+
+The plugin then outputs cross-correlation values in an output text file with
+two columns: the first is an index starting at 1 (we leave the computation
+frequency at R defaults) and the second is the cross-correlation value at that index.
